19c5c15485fb202ee6ccc237da7bfa6415125708
hebertl <lena.hebert@edu.esiee.fr>
2024-11-14 09:02:04 +0000

    ajout de la fonction secondaire dans laquelle nous pouvons faire sorte de comparer la string a l'endroit et al'envers: traduction ou supression des caracteres speciaux puis transformation de la chaine en chaine de miniscule uniquement puis comparaison de la chaine a l'endroit et a l'envers.

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 #### Fonction secondaire
-
+""" test si un mot ou une phrase est palindrome"""
 
 def ispalindrome(p):
+    """ return faux si le mot n'est pas un palindrome"""
+    table = str.maketrans("éèêëàôç","eeeeaoc", " ',-!?:") ###  traduction cas avec accent
+    p = p.lower() ### tranformer chaine de carac en minuscule
+    p = p.translate(table) ### changer tt les carac avec accent en carac sans accent
+    l = len(p)
+    if p[::1]==p[-1:-l-1:-1] : ### mot a l'endroit different du  mot a l'envers
+        return True
 
-    # votre code ici
-    
     return False
 
 #### Fonction principale
 
 
 def main():
-
-    # vos appels à la fonction secondaire ici
+    """ test de la fonction secondaire"""
+    print("bonbon", ispalindrome("bonbon"))# vos appels à la fonction secondaire ici
 
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
         print(s, ispalindrome(s))
